fa8d82c496b5286cc271ddefa7ce9c8a50a516a7
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2021-01-28 00:24:22 +0100

    Move period_contact_t to header file

--- src/contacts.h
+++ src/contacts.h
@@ -13,6 +13,14 @@
 #include "covid_types.h"
 #include "covid.h"
 
+typedef struct period_contact {
+    uint32_t duration;
+    uint16_t cnt;
+    int8_t max_rssi; //TODO also store avg rssi?
+    rolling_proximity_identifier_t rolling_proximity_identifier;
+    associated_encrypted_metadata_t associated_encrypted_metadata;
+} period_contact_t;
+
 void init_contacts();
 int check_add_contact(uint32_t contact_time, rolling_proximity_identifier_t* rpi, associated_encrypted_metadata_t* aem, int8_t rssi);
 void key_change(int current_period_index);
