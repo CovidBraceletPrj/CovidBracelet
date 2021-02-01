4ab9bbe20ebca93bc4dda7bda380f5b204b8d811
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2021-02-01 23:17:48 +0100

    Add store_period_contacts

--- src/storage.h
+++ src/storage.h
@@ -1,9 +1,16 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
+#include "contacts.h"
+
 /**
  * Initialize the storage api.
  */
 int init_storage(void);
+
+/**
+ * Store all contacts of a period in flash.
+ */
+int store_period_contacts(period_contacts_t* contacts);
 
 #endif
