d48245657e8735d8be556dbbb9264d1c3c7f63c0
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2021-02-03 19:39:01 +0100

    get_contacts_for_period

--- src/storage.h
+++ src/storage.h
@@ -11,6 +11,11 @@ int init_storage(void);
 /**
  * Store all contacts of a period in flash.
  */
-int store_period_contacts(period_contacts_t* contacts);
+int store_period_contacts(period_contacts_t contacts);
+
+/**
+ * Get all contacts for a certain period.
+ */
+period_contacts_t get_contacts_for_period(uint16_t period);
 
 #endif
