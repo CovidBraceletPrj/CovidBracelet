1e81fd307cd2f0a6eb4f34fbf3629a9ed3d4b95e
Tobias Schramm <t.schramm@t-sys.eu>
2021-02-08 21:03:52 +0100

    Add powersave and string drawing APIs

--- src/display_pinetime.h
+++ src/display_pinetime.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdbool.h>
 
 int pinetime_display_init();
 void pinetime_display_set_brightness(uint8_t brightness);
+void pinetime_display_draw_string(uint8_t x, uint8_t y, const char *str);
+void pinetime_display_set_powersave(bool powersave);
