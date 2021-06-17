82c2191fb2ef950f1f0c58d094bad56331766afb
Hendrik Sauer <stu213313@mail.uni-kiel.de>
2021-06-17 11:30:57 +0200

    Improve native build

--- zephyr/build_native.sh
+++ zephyr/build_native.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 rm -rf build
-cp ../.pio/libdeps/nrf52840_dk/exposure-notification/*/exposure-notification.* ../src
 west build -b native_posix_64 . -- -DCMAKE_C_FLAGS="-DNATIVE_POSIX -I../../include/tls_config -DDISPLAY"
-rm ../src/exposure-notification.c ../src/exposure-notification.h
 echo "Run ./build/zephyr/zephyr.elf --bt-dev=hci0"
