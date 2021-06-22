ca90702af671dd29357460f85856390704fd43ce
Hendrik Sauer <stu213313@mail.uni-kiel.de>
2021-06-22 16:14:13 +0200

    Fix CMakeLists so Platformio works properly again

--- zephyr/build_native.sh
+++ zephyr/build_native.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf build
-west build -b native_posix_64 . -- -DCMAKE_C_FLAGS="-DNATIVE_POSIX -I../../include/tls_config -DDISPLAY"
+west build -b native_posix_64 . -- -DCMAKE_C_FLAGS="-DNATIVE_POSIX -I../../include/tls_config -DDISPLAY" -DNATIVE_POSIX=1
 echo "Run ./build/zephyr/zephyr.elf"
