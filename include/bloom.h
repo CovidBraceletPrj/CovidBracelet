eb50d1e2007ef4dc581f380d683255d8b283a214
Patrick Rathje <git@patrickrathje.de>
2022-05-31 04:20:40 +0200

    bloom eval

---
+++ include/bloom.h
@@ -0,0 +1,25 @@
+#ifndef BLOOM_H
+#define BLOOM_H
+
+#include "record_storage.h"
+#include "exposure-notification.h"
+
+typedef struct {
+    uint8_t* data;
+    size_t size;
+} bloom_filter_t;
+
+/**
+ * Initialize the bloom filter on basis of the already registerred records.
+ */
+bloom_filter_t* bloom_init(size_t size);
+
+void bloom_destroy(bloom_filter_t* bloom);
+
+// TODO lome: maybe only use RPI (should be sufficient)
+void bloom_add_record(bloom_filter_t* bloom, ENIntervalIdentifier* rpi);
+
+// TODO lome: maybe only use RPI (should be sufficient)
+bool bloom_probably_has_record(bloom_filter_t* bloom, ENIntervalIdentifier* rpi);
+
+#endif
